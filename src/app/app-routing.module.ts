7f6611f9c6f7977d455546b17dec131beec6783d
Yuri Barbosa <yuri.barbosa@gmail.com>
2020-08-15 16:28:24 -0300

    Definição da rota e criação do HTML do componente que lista as tarefas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
-
+import { TarefaRoutes } from './tarefas';
 
 export const routes: Routes = [
-
+    {
+        path: '',
+        redirectTo: '/tarefas/listar',
+        pathMatch: 'full'
+    },
+    ...TarefaRoutes
 ];
 
 @NgModule({
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarefaService } from './shared';
+import { ListarTarefasComponent } from './listar';
 
 @NgModule({
-  declarations: [],
+  declarations: [ListarTarefasComponent],
   imports: [
     CommonModule
   ],
